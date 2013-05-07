56e7e13f488583a4d065ec5500e20d33fbe95be7
Walker Hamilton <myself@walkerhamilton.com>
2013-05-07 09:40:46 -0500

    Move (c) to 2013

--- ngp-donations.php
+++ ngp-donations.php
@@ -9,7 +9,7 @@
 	Tags: NGP, NGPVAN, Voter Action Network, donations, FEC, politics, fundraising
 	License: MIT
 	
-	Copyright 2011 Revolution Messaging LLC (email: support@revolutionmessaging.com)
+	Copyright 2013 Revolution Messaging LLC (email: support@revolutionmessaging.com)
 	
 	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 	
